8734f5532e9e6840869bac5eb9ac280b7e6918b5
Zhaoqi Wang <max0829@126.com>
2022-05-26 16:35:25 -0400

    Added a loop to avoid reading the json twice, organized the print function

--- src/search.py
+++ src/search.py
@@ -26,6 +26,22 @@ def Store_Matches():
         Hitted_ids.append(hit['id'])
         Hitted_recipe_names.append(hit["name"])
 
+def print_result(results):
+    # print the mactched recipe id and the name of the recipes (range(len(results)))
+    print("----------------------------------")
+    print("Matched results,\n#.(Recipe name, id)")
+
+    for hit in range(0, len(Hitted_recipe_names)):
+        print(str(hit+1) + "." +
+            "(" + Hitted_recipe_names[hit] + ", " + Hitted_ids[hit] + ")")
+
+    # print the stats
+    # Note: The Results object acts like a list of the matched documents. You can use it to access the stored fields of each hit document, to display to the user.
+    print("----------------------------------")
+    print("The stats")
+    print("number of results:", len(results))
+    print("Searching stats:", results)
+
 
 # schema = Schema(
 #     ingredients=TEXT(stored=True),
@@ -36,9 +52,9 @@ def Store_Matches():
 #     instructions=TEXT(stored=True)
 # )
 # set up schema
-''' Note: If you aren’t specifying any constructor keyword arguments to one of the predefined fields,
-you can leave off the brackets (e.g. fieldname=TEXT instead of fieldname=TEXT()).
-Whoosh will instantiate the class for you.'''
+    ''' Note: If you aren’t specifying any constructor keyword arguments to one of the predefined fields,
+    you can leave off the brackets (e.g. fieldname=TEXT instead of fieldname=TEXT()).
+    Whoosh will instantiate the class for you.'''
 schema = Schema(
     name=TEXT(stored=True, analyzer=StemmingAnalyzer()),
     id=ID(stored=True)
@@ -53,40 +69,33 @@ ix = index.create_in("indexdir", schema)
 writer = ix.writer()
 
 # open json, loop through list and add each recipe object as a document to the index
-x = open('partial_layer1.json')
+x = open('layer1.json')
 layer1 = json.load(x)
 for i in range(0, len(layer1)):
     writer.add_document(name=layer1[i]["title"], id=layer1[i]["id"])
 
 writer.commit()
 
-# takes in name of the default field to search, and the name of the schema
-qp = QueryParser("name", schema=ix.schema)
-user_query = input("Search recipe: ")
-q = qp.parse(user_query)
+#Infinite loop to avoid reading json repetitively 
+open = True
+while(open):
+    # takes in name of the default field to search, and the name of the schema
+    qp = QueryParser("name", schema=ix.schema)
+    print("Type 0 if you want to stop the search.")
+    user_query = input("Search recipe: ")
+    if user_query == '0':
+        print("Thank you for using the search tool")
+        break
+    q = qp.parse(user_query)
 
-# index searcher object
-with ix.searcher() as searcher:
-    # search() takes a whoosh.query.Query object and returns a Results object
-    '''Note: We will need to limit the result for recipe (around 5 to 10) (Discuss 1,5,10)
-    Note: it looks like that we can only read the results from here, 
-    otherwise it will say that the reader is closed'''
+    # index searcher object
+    with ix.searcher() as searcher:
+        # search() takes a whoosh.query.Query object and returns a Results object
+        '''Note: We will need to limit the result for recipe (around 5 to 10) (Discuss 1,5,10)
+        Note: it looks like that we can only read the results from here, 
+        otherwise it will say that the reader is closed'''
 
-    results = searcher.search(q, limit=None)
-    Store_Matches()
+        results = searcher.search(q, limit=10)
+        Store_Matches()
+        print_result(results)
 
-
-# print the mactched recipe id and the name of the recipes (range(len(results)))
-print("----------------------------------")
-print("Matched results,\n#.(Recipe name, id)")
-
-for hit in range(0, len(Hitted_recipe_names)):
-    print(str(hit+1) + "." +
-          "(" + Hitted_recipe_names[hit] + ", " + Hitted_ids[hit] + ")")
-
-# print the stats
-# Note: The Results object acts like a list of the matched documents. You can use it to access the stored fields of each hit document, to display to the user.
-print("----------------------------------")
-print("The stats")
-print("number of results:", len(results))
-print("Searching stats:", results)
