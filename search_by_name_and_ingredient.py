e5e942ae664c90829ac15ff2e255fa5c3077437b
CodefromMax <max0829@126.com>
2022-05-30 22:57:48 -0400

    Improved on modularity, and printed the relevant recipe data

--- search_by_name_and_ingredient.py
+++ search_by_name_and_ingredient.py
@@ -1,4 +1,4 @@
-
+from __future__ import print_function
 import json
 from whoosh import index, query
 from whoosh.fields import Schema, TEXT, KEYWORD, ID, STORED
@@ -14,56 +14,85 @@ from whoosh.qparser import QueryParser, OrGroup
 Hitted_ids = list()
 Hitted_recipe_names = list()
 
-#Print Menu
-#print("---------------------")
+#User Menu
+def PrintMenu():
+    print("---------------------")
+    print("Type '1' for search by recipe name")
+    print("Type '2' for seach by ingredient")
+    print("Type '0' to end")
+
 
 
 # Store the results, id and recipe name into two list
-def Store_Matches():
+def Store_Matches(results):
     global Hitted_ids
     global Hitted_recipe_names
+    global Hitted_ingredients
     global Hitted_nutri
+
     # Refresh
     Hitted_ids = list()
     Hitted_recipe_names = list()
+    Hitted_ingredients = list()
     Hitted_nutri = list()
+    
+    #Store the results
     for hit in results:
         Hitted_ids.append(hit['id'])
         Hitted_recipe_names.append(hit["name"])
+        Hitted_ingredients.append(hit["ingredients"])
         Hitted_nutri.append(hit["nutri_info"])
 
+#Print the results
 def print_result(results):
+    
     # print the mactched recipe id and the name of the recipes (range(len(results)))
     print("----------------------------------")
-    print("Matched results,\n#.(Recipe name, id, nutritional data)")
+    print("Matched results,\n#.( Recipe name, ingredients, nutritional data)")
 
     for hit in range(0, len(Hitted_recipe_names)):
         print(str(hit+1) + "." +
-            "(" + Hitted_recipe_names[hit] + ", " + Hitted_ids[hit] + ", " + Hitted_nutri[hit] + ")")
+            "(" + Hitted_ids[hit] + "," + "name:" + Hitted_recipe_names[hit] + ", ingredients " + Hitted_ingredients[hit] + ", nutritional data" + Hitted_nutri[hit] + ")")
 
     # print the stats
-    # Note: The Results object acts like a list of the matched documents. You can use it to access the stored fields of each hit document, to display to the user.
     print("----------------------------------")
     print("The stats")
     print("number of results:", len(results))
     print("Searching stats:", results)
 
-# def search_ingredients():
-# while(True):
-#     answer = input("Do you want to search by ingredients? (y = yes, n = no)")
-#     if answer == "y" or "Y":
-#         queryParser("ingredients",ix.schema)
-#         user_query("Type in the sepcific ingredient, if more than one use ',' to separte")
-#         break
-#     elif answer == "n" or "N":
-#         break
-#     else:
-#         print("Wrong answer")
+# Search by fields
+def sTool(first, second):
+    
+    #Infinite loop to avoid reading json repetitively 
+    open = True
+    while(open):
+        
+        # takes in name of the default field to search, and the name of the schema
+        qp = QueryParser(first, schema=ix.schema)
+        print("--------------------------\nType 0 if you want to go to menu.")
+        user_query = input("Search by " + first + ": ")
+        if user_query == '0':
+            print("Thank you for using the search tool")
+            break
 
+        q = qp.parse(user_query)
+        
+        # index searcher object
+        with ix.searcher() as searcher:
 
+            while(True):
+                answer = input("Do you want to search by "+ second+ " ? (y = yes, n = no):")
+                if answer == "y" or answer == "Y":
+                    results = searcher.search(q, limit=10,filter = query.Term(second,input("Type in the sepcific " + second + ", (if more than one use ',' to separte)\n")))
+                    break
+                elif answer == "n" or answer == "N":
+                    results = searcher.search(q, limit=10)
+                    break
+                else:
+                    print("Wrong answer")
 
-
-
+            Store_Matches(results)
+            print_result(results)
 
 
 
@@ -75,10 +104,8 @@ def print_result(results):
 #     id=ID(stored=True),
 #     instructions=TEXT(stored=True)
 # )
+
 # set up schema
-    ''' Note: If you aren’t specifying any constructor keyword arguments to one of the predefined fields,
-    you can leave off the brackets (e.g. fieldname=TEXT instead of fieldname=TEXT()).
-    Whoosh will instantiate the class for you.'''
 schema = Schema(
     name=TEXT(stored=True, analyzer=StemmingAnalyzer()),
     id=ID(stored=True),
@@ -91,11 +118,12 @@ if not os.path.exists("indexdir"):
     os.mkdir("indexdir")
 
 ix = index.create_in("indexdir", schema)
+
 # get index writer object
 writer = ix.writer()
 
 # open json, loop through list and add each recipe object as a document to the index
-x = open('recipes_with_nutritional_info.json')
+x = open('/Users/maxwang/Desktop/json/recipes_with_nutritional_info.json')
 layer1 = json.load(x)
 for i in range(0, len(layer1)):
     ingredients1 = ""
@@ -106,49 +134,23 @@ for i in range(0, len(layer1)):
 
 writer.commit()
 
-#Infinite loop to avoid reading json repetitively 
-open = True
-
-
-
-while(open):
-    ingredients_search = True
-    # takes in name of the default field to search, and the name of the schema
-    qp = QueryParser("name", schema=ix.schema)
-    print("Type 0 if you want to stop the search.")
-    user_query = input("Search recipe: ")
-    if user_query == '0':
-        print("Thank you for using the search tool")
+# get chocie
+while(True):
+    
+    #Print menu
+    PrintMenu()
+    user_choice = input("choice:")
+    
+    #Seach by name first
+    if user_choice == "1":
+        sTool("name", "ingredients")
+    
+    #Search by ingredient first
+    elif user_choice == "2":
+        sTool("ingredients","name")
+    
+    #end the search
+    elif user_choice == "0":
         break
-
-    q = qp.parse(user_query)
-    # index searcher object
-    with ix.searcher() as searcher:
-
-        while(True):
-            answer = input("Do you want to search by ingredients? (y = yes, n = no):")
-            if answer == "y" or answer == "Y":
-                results = searcher.search(q, limit=10,filter = query.Term("ingredients",input("Type in the sepcific ingredient, (if more than one use ',' to separte)\n")))
-                #allow_q = query.Term("ingredients",input("Type in the sepcific ingredient, if more than one use ',' to separte"))
-                break
-            elif answer == "n" or answer == "N":
-                results = searcher.search(q, limit=10)
-                break
-            else:
-                print("Wrong answer")
-
-        Store_Matches()
-        print_result(results)
-
-
-
-    # # index searcher object
-    # with ix.searcher() as searcher:
-    #     # search() takes a whoosh.query.Query object and returns a Results object
-    #     '''Note: We will need to limit the result for recipe (around 5 to 10) (Discuss 1,5,10)
-    #     Note: it looks like that we can only read the results from here, 
-    #     otherwise it will say that the reader is closed'''
-
-    #     results = searcher.search(q, limit=10,filter = allow_q)
-    #     Store_Matches()
-    #     print_result(results)
+    else:
+        print("wrong answer")
